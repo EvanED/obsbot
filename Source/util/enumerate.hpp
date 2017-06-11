72e0f625ca3ddc5cb66b7d1e589461ef2b34625c
Evan Driscoll <evaned@gmail.com>
2017-06-11 17:50:38 -0500

    Add currently-unused zip function

--- Source/util/enumerate.hpp
+++ Source/util/enumerate.hpp
@@ -11,3 +11,17 @@ auto enumerate(T && sequence)
 		++n;
 	}
 }
+
+template <typename T1, typename T2>
+auto zip(T1 && seq1, T2 && seq2)
+{
+	auto item1 = std::begin(seq1);
+	auto item2 = std::begin(seq2);
+
+	auto end1 = std::end(seq1);
+	auto end2 = std::end(seq2);
+
+	for (; item1 != end1 && item2 != end2; ++item1, ++item2) {
+		co_yield std::make_pair(*item1, *item2);
+	}
+}
